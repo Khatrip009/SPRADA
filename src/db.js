3319253a53d0ef5fdb892ffb840b21f4a88394b8
Khatrip009 <Khatrip.009@gmail.com>
2026-01-29 11:27:28 +0530

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,17 +2,18 @@ const { Pool } = require('pg');
 const dotenv = require('dotenv');
 dotenv.config();
 
-
 const pool = new Pool({
-connectionString: process.env.DATABASE_URL,
-max: 20,
-idleTimeoutMillis: 30000,
-connectionTimeoutMillis: 2000
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000
 });
 
-
 pool.on('error', (err) => {
-console.error('Unexpected error on idle pg client', err);
+  console.error('Unexpected error on idle pg client', err);
 });
 
 console.log("🔵 Using DATABASE_URL:", process.env.DATABASE_URL);
