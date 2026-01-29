83a6c8a28ec156d8654a18593919aa7a33a084e9
Khatrip009 <Khatrip.009@gmail.com>
2026-01-29 18:18:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -34,14 +34,7 @@ app.use(compression());
 app.use(express.json({ limit: '16mb' }));
 app.use(express.urlencoded({ extended: true, limit: '16mb' }));
 
-/* ---------------- FILE UPLOAD (REQUIRED FOR SUPABASE) ---------------- */
-app.use(
-  fileUpload({
-    limits: { fileSize: 2 * 1024 * 1024 }, // 2MB
-    abortOnLimit: true,
-    createParentPath: false
-  })
-);
+
 
 /* ---------------- LOGGING ---------------- */
 app.use(morgan(ENV === 'production' ? 'combined' : 'dev'));
